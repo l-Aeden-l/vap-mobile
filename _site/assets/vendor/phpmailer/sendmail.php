e6cd8d07cf42143cf39981cbe3a203c9423e758e
Aeden <hauguel.frederic@gmail.com>
2020-09-05 15:22:33 +0200

    Correction envoi mail php

--- _site/assets/vendor/phpmailer/sendmail.php
+++ _site/assets/vendor/phpmailer/sendmail.php
@@ -1,13 +1,37 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
 
-$to = "hauguel.frederic@gmail.com";
-$subject = "Formulaire de contact Vap'Mobile - Message de $name";
-$message = "Madame ou monsieur $name a laissé pour vous le message suivant : <br><br>$message<br><br>Vous pouvez la/le joindre au numéro suivant<br><br>$phone"; 
-$from = "contact@vap-mobile.fr";
+if (!empty($_POST)) {
 
-// Envoi d'email
-if(mail($to, $subject, $message)){
-    echo "Votre message a été envoyé avec succès!";
-} else{
-    echo "Impossible d'envoyer des emails. Veuillez réessayer.";
+  // Champs formulaire
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+
+  // Création d'un nouvel objet $mail
+  $mail = new PHPMailer();
+  // Encodage
+  $mail->CharSet = 'UTF-8';
+
+  // Corp de notre email
+  $body = "Madame ou monsieur $name a laissé pour vous le message suivant : <br><br>$message<br><br>Vous pouvez la/le joindre au numéro suivant<br><br>$phone";
+  // Expediteur, adresse de retour et destinataire :
+  $mail->SetFrom("contact@vap-mobile.fr", "Formulaire de contact Vap'Mobile");
+  $mail->AddReplyTo($email, $name);
+  $mail->AddAddress("hauguel.frederic@gmail.com", "Jérôme HAUGUEL - Vap'Mobile");
+  // Sujet du mail
+  $mail->Subject = "Formulaire de contact Vap'Mobile - Message de $name";
+  // Le message
+  $mail->MsgHTML($body);
+  // Envoi de l'email
+  if ( !$mail->Send() ) {
+    echo "Echec de l'envoi du mail, Erreur: " . $mail->ErrorInfo;
+  } else {
+    echo "Message envoyé!";
+  }
+  unset($mail);
 }
