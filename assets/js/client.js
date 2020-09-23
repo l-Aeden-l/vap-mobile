c069446def5ef5c796fdb6e51d7cf3829628bb0e
Aeden <hauguel.frederic@gmail.com>
2020-09-23 18:08:44 +0200

    Ajout plugin Facebook Customer Chat

---
+++ assets/js/client.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '671918130121208',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v8.0'
+    });
+  };
+
+  FB.CustomerChat.show(shouldShowDialog: boolean);
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+});
